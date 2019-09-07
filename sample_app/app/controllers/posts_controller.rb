35930e2fe476b717e408171ac6128c38b1a78ed1
田中　大樹 <tanakadaiki@tanakataijunoMacBook-Pro.local>
2019-09-07 14:44:31 +0900

    [Finish]3-6

--- sample_app/app/controllers/posts_controller.rb
+++ sample_app/app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def create
   	post = Post.new(post_params)
   	post.save
-  	redirect_to '/top'
+  	redirect_to post_path(post.id)
   end
 
   def index
@@ -15,6 +15,16 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
   end
 	private
 	
